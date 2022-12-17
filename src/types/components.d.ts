0f2e6d90f6560ed22a4e2e35dd7beca1c28eff5c
kaivanwong <kaivanwong@outlook.com>
2022-12-17 17:04:09 +0800

    feat: 更新分布表单

--- src/types/components.d.ts
+++ src/types/components.d.ts
@@ -64,7 +64,6 @@ declare module '@vue/runtime-core' {
     ElTabPane: typeof import('element-plus/es')['ElTabPane']
     ElTabs: typeof import('element-plus/es')['ElTabs']
     ElTag: typeof import('element-plus/es')['ElTag']
-    ElTimePicker: typeof import('element-plus/es')['ElTimePicker']
     ElTooltip: typeof import('element-plus/es')['ElTooltip']
     ElUpload: typeof import('element-plus/es')['ElUpload']
     LayoutAdmin: typeof import('./../components/layout/layout-admin.vue')['default']
