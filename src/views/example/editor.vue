bfae23fe0a84ceca245a6a9a76185be90c1bc8d4
kaivanwong <kaivanwong@outlook.com>
2022-12-24 17:37:55 +0800

    feat: add the map and editor;

--- src/views/example/editor.vue
+++ src/views/example/editor.vue
@@ -1,8 +1,52 @@
 <script lang="ts" setup>
+import '@wangeditor/editor/dist/css/style.css';
+import { Editor, Toolbar } from '@wangeditor/editor-for-vue';
+
 defineOptions({
   name: 'ExampleEditor',
 });
+
+const mode = 'default';
+
+const editorRef = shallowRef();
+
+const valueHtml = ref('<p>hello</p>');
+
+onMounted(() => {
+  setTimeout(() => {
+    valueHtml.value = '<p>模拟 Ajax 异步设置内容</p>';
+  }, 1500);
+});
+
+const toolbarConfig = {};
+const editorConfig = { placeholder: '请输入内容...' };
+
+onBeforeUnmount(() => {
+  const editor = editorRef.value;
+  if (editor == null) return;
+  editor.destroy();
+});
+
+const handleCreated = (editor: any) => {
+  editorRef.value = editor;
+};
 </script>
 <template>
-  <crud-card></crud-card>
+  <crud-card>
+    <div style="border: 1px solid #ccc">
+      <Toolbar
+        style="border-bottom: 1px solid #ccc"
+        :editor="editorRef"
+        :default-config="toolbarConfig"
+        :mode="mode"
+      />
+      <Editor
+        style="height: 500px; overflow-y: hidden"
+        v-model="valueHtml"
+        :default-config="editorConfig"
+        :mode="mode"
+        @on-created="handleCreated"
+      />
+    </div>
+  </crud-card>
 </template>
