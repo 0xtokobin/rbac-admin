8b74672b72174959d0bde8e121aae889882543c5
kaivanwong <kaivanwong@outlook.com>
2023-03-22 13:35:26 +0800

    update

--- packages/common/index.ts
+++
@@ -1,9 +0,0 @@
-import { bootstrap as admin } from 'packages/admin/src/main'
-import { bootstrap as oa } from 'packages/oa/src/main'
-
-export const bootstrap = (type: string) => {
-  if (type === 'admin')
-    admin()
-  else if (type === 'oa')
-    oa()
-}
--- scripts/dev.ts
+++
@@ -1,5 +0,0 @@
-import { execSync as exec } from 'node:child_process'
-
-const [,,app, env] = process.argv
-
-exec(`pnpm vite --config ./scripts/${app}/index.ts --mode ${env}`)
