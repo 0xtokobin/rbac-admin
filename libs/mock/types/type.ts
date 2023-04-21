5fe81eb066b856b1d9912d08c64ff70df836aaed
kaivanwong <kaivanwong@outlook.com>
2023-04-21 14:50:05 +0800

    feat: remove root vite config

--- libs/mock/types/type.ts
+++
@@ -1,7 +0,0 @@
-export interface Files<T = any> {
-  [key: string]: T
-}
-
-export interface Mocks<T = any> {
-  [key: string]: T
-}
--- types/declare.d.ts
+++ types/declare.d.ts
@@ -7,11 +7,7 @@ declare module '*.bmp';
 declare module '*.tiff';
 declare module '*.gif';
 declare module 'qs';
-declare module 'mockjs';
 declare module 'tsparticles';
-declare module 'unplugin-vue-define-options/vite';
 declare module 'element-plus/dist/locale/en.mjs';
 declare module 'element-plus/dist/locale/zh-cn.mjs';
 declare module '@element-plus/icons-vue'
-
-
